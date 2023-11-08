52bc14bbee7db925962bd91de890095ac41c8c37
Davit Putra Hartono <davit.putra.ft21@mail.umy.ac.id>
2023-11-08 15:01:26 +0700

    first commit

---
+++ app/src/main/java/com/example/meet_7/Data/OrderUIState.kt
@@ -0,0 +1,7 @@
+package com.example.meet_7.Data
+
+data class OrderUIState(
+    val jumlah: Int = 0,
+    val rasa: String = "",
+    val harga: String = ""
+)
---
+++ app/src/main/java/com/example/meet_7/Data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.meet_7.Data
+
+import com.example.meet_7.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanila,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche
+    )
+}
